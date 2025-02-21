4c5cb8a6857dea579a127ae3aaeb9ac52fef210b
Parapet Cloud <parapetcloud@live.com>
2025-02-20 20:51:50 -0800

    Put this back where it belongs

--- VTS/UnitGroup.cs
+++ VTS/UnitGroup.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VtolVrRankedMissionSetup.VT;
-using VtolVrRankedMissionSetup.VTS;
 
-namespace VtolVrRankedMissionSetup
+namespace VtolVrRankedMissionSetup.VTS
 {
     public class UnitGroup
     {
